257e25cd66925ceff2cf0ea2f6256604eff8be9c
Wondle <54881779+ELNtEL@users.noreply.github.com>
2025-12-28 21:02:28 +0100

    fix railway

--- config/settings.py
+++ config/settings.py
@@ -18,8 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ==============================================================================
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-fallback-railway-temp-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default='False', cast=bool)
 
