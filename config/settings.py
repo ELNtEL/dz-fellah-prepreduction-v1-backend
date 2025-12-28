2014843c89711e1c673863ec69f73577f6758ec8
Wondle <54881779+ELNtEL@users.noreply.github.com>
2025-12-28 23:45:06 +0100

    V1-3

--- config/settings.py
+++ config/settings.py
@@ -298,6 +298,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://127.0.0.1:3000',
     'http://127.0.0.1:5173',
+    'https://dz-fellah-prepreduction-v1-frontend-rho.vercel.app'
 ]
 
 # Add production frontend URL if specified
